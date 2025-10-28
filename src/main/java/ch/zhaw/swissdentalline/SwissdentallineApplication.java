fa53e87a5d8a64058040d90c14d68289375145c5
EndritKukalaj <ekukalaj@hotmail.com>
2025-10-28 11:36:02 +0100

    first commit

---
+++ src/main/java/ch/zhaw/swissdentalline/SwissdentallineApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.swissdentalline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissdentallineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwissdentallineApplication.class, args);
+	}
+
+}
