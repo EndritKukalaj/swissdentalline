ae4271ad5111c5237a63d5c0d130912ffbcd5168
EndritKukalaj <ekukalaj@hotmail.com>
2025-11-19 23:34:55 +0100

    feat: initialize frontend project with SvelteKit and Vite. Closes #16

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+</script>
+
+<h1>Willkommen bei SwissDentalLine</h1>
+
+<!-- Beispiel für die Verwendung von Bildern im Ordner 'static/images' -->
+<img src="/images/vite.svg" alt="Vite Logo" />
